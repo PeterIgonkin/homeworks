4db6e08e91abc9004fcb1ffd0e70710fa84d1e4b
PeterIgonkin <petr-igonkin@yandex.ru>
2021-10-03 17:32:20 +0300

    first

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int a;
+	cin >> a;
+	if (a >= 100 && a < 1000)
+	{
+		cout << "Число трехзначное" << endl;
+
+	}
+	else
+	{
+		cout << "Число не трехзначное" << endl;
+	}
+	system("pause");
+	return 0;
+}
