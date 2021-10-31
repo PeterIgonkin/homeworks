dc523b3bc6d0dc2c4f918907dd3f1c2236c117b1
PeterIgonkin <petr-igonkin@yandex.ru>
2021-10-31 16:31:38 +0300

    Create zadanie2.cpp

---
+++ zadanie2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+void cubes() {
+	cout << rand() % 6 + 1 << endl;
+	cout << rand() % 6 + 1 << endl;
+}
+void fibonacci(unsigned int n) {
+	unsigned int pre = 0;
+	unsigned int now = 1;
+	for (int i = 0; i < n - 2; i++) {
+		unsigned int temp = pre + now;
+		pre = now;
+		now = temp;
+	}
+
+	if (n == 0) {
+		cout << 0;
+	}
+	else {
+		cout << now;
+	}
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	cubes();
+	unsigned int n;
+	cout << "¬ведите число: ";
+	cin >> n;
+	fibonacci(n);
+	return 0;
+}
