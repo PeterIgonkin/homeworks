28da49400e35e3dce8d72840e852417f05f5a8ec
PeterIgonkin <petr-igonkin@yandex.ru>
2021-11-02 04:18:44 +0300

    Update zadanie2.cpp

--- zadanie2.cpp
+++ zadanie2.cpp
@@ -6,19 +6,39 @@ void cubes() {
 	cout << rand() % 6 + 1 << endl;
 }
 void fibonacci(unsigned int n) {
-	unsigned int pre = 0;
-	unsigned int now = 1;
-	for (int i = 0; i < n - 2; i++) {
-		unsigned int temp = pre + now;
-		pre = now;
-		now = temp;
-	}
-
 	if (n == 0) {
 		cout << 0;
 	}
+	else if (n == 1) {
+		cout << 1;
+	}
 	else {
+		unsigned int pre = 0;
+		unsigned int now = 1;
+		for (int i = 0; i < n - 2; i++) {
+			unsigned int temp = pre + now;
+			pre = now;
+			now = temp;
+		}
 		cout << now;
+	}
+}
+int summa(int n) {
+	int sum = 0;
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0) {
+			sum += i;
+		}
+	}
+	return sum;
+}
+void pairs() {
+	for (int i = 1; i < 10000; i++) {
+		for (int j = 1; j < 10000; j++) {
+			if ((i != j) && (summa(i) == j) && (summa(j) == i)) {
+				cout << "Дружественная пара: " << i << ' ' << j << endl;
+			}
+		}
 	}
 }
 int main() {
@@ -29,5 +49,7 @@ int main() {
 	cout << "Введите число: ";
 	cin >> n;
 	fibonacci(n);
+	cout << endl;
+	pairs();
 	return 0;
 }
